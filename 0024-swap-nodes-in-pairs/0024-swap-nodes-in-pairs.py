99dbba811ae0a3891703fb478511f2a3aaed60b3
Farhia_Afroj <166537336+FarhiaAfroj@users.noreply.github.com>
2025-07-31 16:19:41 +0600

    Time: 0 ms (100%), Space: 12.5 MB (34.45%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -0,0 +1,24 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def swapPairs(self, head):
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+        
+        while current.next and current.next.next:
+            first = current.next
+            second = current.next.next
+            
+            # Swapping nodes
+            first.next = second.next
+            second.next = first
+            current.next = second
+            
+            # Move current pointer for next swap
+            current = first
+        
+        return dummy.next
