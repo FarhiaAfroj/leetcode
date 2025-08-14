a8a7988364166350f66ee72c7d7d641bd89bdc61
Farhia_Afroj <166537336+FarhiaAfroj@users.noreply.github.com>
2025-08-14 23:05:35 +0600

    Time: 0 ms (100%), Space: 17.9 MB (95.43%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -4,14 +4,14 @@ class Solution:
             return 0
 
         ans = 0
-        # Check if left child exists and is a leaf
+       
         if root.left and not root.left.left and not root.left.right:
             ans += root.left.val
         else:
-            # Otherwise, recurse into left subtree
+           
             ans += self.sumOfLeftLeaves(root.left)
 
-        # Always recurse into right subtree
+        
         ans += self.sumOfLeftLeaves(root.right)
 
         return ans
