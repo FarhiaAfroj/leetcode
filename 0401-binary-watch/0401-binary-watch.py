10b1a7d1bd3343b3db2cfb1473dc819358c54a3c
Farhia_Afroj <166537336+FarhiaAfroj@users.noreply.github.com>
2025-08-14 23:00:52 +0600

    Time: 0 ms (100%), Space: 17.7 MB (73.02%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,8 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> list[str]:
+        ans = []
+        for hour in range(12):
+            for minute in range(60):
+                if hour.bit_count() + minute.bit_count() == turnedOn:
+                    ans.append(f"{hour}:{minute:02d}")
+        return ans
