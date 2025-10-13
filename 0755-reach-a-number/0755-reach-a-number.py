15d335d37720dc707967909ee9ff5ad25f0c5373
Farhia_Afroj <166537336+FarhiaAfroj@users.noreply.github.com>
2025-10-13 17:22:56 +0600

    Time: 31 ms (56.88%), Space: 17.9 MB (38.04%) - LeetHub

---
+++ 0755-reach-a-number/0755-reach-a-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        target = abs(target)
+        n = 0
+        total = 0
+        
+        while total < target or (total - target) % 2 != 0:
+            n += 1
+            total += n
+        
+        return n
+        
