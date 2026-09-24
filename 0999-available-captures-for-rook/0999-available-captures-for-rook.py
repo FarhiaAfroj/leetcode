da91ebe063dd15daa40c82cd03119da8699f46be
Farhia_Afroj <166537336+FarhiaAfroj@users.noreply.github.com>
2026-09-24 20:47:09 +0600

    Time: 0 ms (100%), Space: 19.3 MB (73.95%) - LeetHub

---
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numRookCaptures(self, board: List[List[str]]) -> int:
+        for i in range(8):
+            for j in range(8):
+                if board[i][j] == 'R':
+                    r, c = i, j
+
+        count = 0
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+
+            while 0 <= nr < 8 and 0 <= nc < 8:
+                if board[nr][nc] == 'B':
+                    break
+
+                if board[nr][nc] == 'p':
+                    count += 1
+                    break
+
+                nr += dr
+                nc += dc
+
+        return count
