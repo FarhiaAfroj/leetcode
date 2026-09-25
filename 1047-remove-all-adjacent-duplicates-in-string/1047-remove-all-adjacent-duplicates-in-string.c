b81a4903f5f0a36ee97f21e0d3adde78e6cc833e
Farhia_Afroj <166537336+FarhiaAfroj@users.noreply.github.com>
2026-09-25 23:14:49 +0600

    Time: 0 ms (100%), Space: 10.1 MB (84.79%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+char* removeDuplicates(char* s) {
+    int top = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (top > 0 && s[top - 1] == s[i]) {
+            top--;
+        } else {
+            s[top++] = s[i];
+        }
+    }
+
+    s[top] = '\0';
+    return s;
+}
